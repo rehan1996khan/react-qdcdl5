4d569339fd1684ef09b6edc0d58f703ced88b925
rehan1996khan <rehankhan30647@gmail.com>
2022-04-17 12:47:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-qdcdl5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qdcdl5)
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <>
+      <div className="Hello-img">
+        <div className="h1-img">
+          <h1>Hello StackBlitz!</h1>
+        </div>
+        <div className="p-img">
+          <p>
+            ' Start editing to see some magic happen :) ddsad ssasxasxa saasass
+            sasdasas saasssssssssssssssssssssssssss
+            ssssssssssssssssssssssssssssssssssssssssss
+            ssssssssssssssssssssssssssssssssssssssssss ssssssssssssssssssssss
+            ssssssssssssssssssssssssssssssssssss
+            ssssssssssssssssssssssssssssssssssssssssss
+            ssssssssssssssssssssssssssssssssssssssssssssss
+            ssssssssssssssssssssssssssssssssssssssssssssss
+            ssssssssssssssssssssssssssssssssssssssssssss
+            ssssssssssssssssssssssskjzxksjkkncska4 kkkkkkkkkkkkkkkkkk
+            kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+            kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+            kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+            kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk kkkkkkkkkkkk
+            kkkkkkkkkkkkkkkkkkkkkkkkkkk'
+          </p>
+        </div>
+      </div>
+      <div>
+        <input className="input-img" type="password" />
+      </div>
+    </>
+  );
+}
